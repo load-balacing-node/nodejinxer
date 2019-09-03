5129aa9cc0a9b0c0185b2b8c6c52e9ae4a13eb6e
Nestor hdez <nestorhdez.26@gmail.com>
2019-09-03 16:25:54 +0100

    Render the request of the API.

--- nodejinxer-nginx/src/main.js
+++ nodejinxer-nginx/src/main.js
@@ -2,10 +2,8 @@ const getBtn = document.getElementById('get-btn');
 const container = document.getElementById('req-container');
 
 getBtn.addEventListener('click', async function(){
-    await fetch('http://localhost:3000/api')
+    await fetch('http://localhost:8080/api')
         .then(res => res.json())
-        .then(res => {
-            console.log(res)
-        })
+        .then(res => res.forEach(obj => container.innerHTML += `<p>${obj.name}</p>`))
         .catch(() => container.innerHTML = '<h2 id="fail" style="color: red;">Something wrong happend... Try it again</h2>')
 });
