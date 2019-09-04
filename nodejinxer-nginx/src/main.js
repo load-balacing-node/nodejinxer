a47deecc49422210a1ef0d69e618cd5bd34e118c
Marcello Graffigna <marcesmartphone@gmail.com>
2019-09-04 19:17:08 +0100

    localhost support

--- nodejinxer-nginx/src/main.js
+++ nodejinxer-nginx/src/main.js
@@ -1,8 +1,8 @@
 const getBtn = document.getElementById('get-btn');
 const container = document.getElementById('req-container');
 
-getBtn.addEventListener('click', async function(){
-    await fetch('http://ec2-34-255-118-172.eu-west-1.compute.amazonaws.com:8080/api')
+getBtn.addEventListener('click', async function () {
+    await fetch('http://localhost:8080/api')
         .then(res => res.json())
         .then(res => res.forEach(obj => container.innerHTML += `<p>${obj.name}</p>`))
         .catch(() => container.innerHTML = '<h2 id="fail" style="color: red;">Something wrong happend... Try it again</h2>')
